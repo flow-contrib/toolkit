1f9a0f312de2ac25f9dbf332c317997def6c7674
Zeal <xujinzheng@gmail.com>
2018-04-28 18:50:11 +0800

    fix pg support, add tx support

--- sql/sql.go
+++ sql/sql.go
@@ -16,6 +16,7 @@ var (
 )
 
 type sqlConfig struct {
+	Driver   string
 	User     string
 	Password string
 	Host     string
@@ -26,8 +27,17 @@ type sqlConfig struct {
 }
 
 func (p *sqlConfig) DSN() string {
-	loc := strings.Replace(p.Location, "/", "%2F", 1)
-	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&loc=%s", p.User, p.Password, p.Host, p.Port, p.Db, p.Charset, loc)
+	if p.Driver == "mysql" {
+		loc := strings.Replace(p.Location, "/", "%2F", 1)
+		return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&loc=%s", p.User, p.Password, p.Host, p.Port, p.Db, p.Charset, loc)
+	} else if p.Driver == "postgres" {
+		if len(p.Db) == 0 {
+			p.Db = "postgres"
+		}
+		return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", p.Host, p.Port, p.User, p.Password, p.Db)
+	}
+
+	panic("unsupport driver:" + p.Driver)
 }
 
 func init() {
@@ -42,6 +52,7 @@ func Query(ctx context.Context, conf config.Configuration) (err error) {
 	}
 
 	sqlConf := sqlConfig{
+		Driver:   conf.GetString("driver", "mysql"),
 		Host:     conf.GetString("host", "localhost"),
 		Port:     int(conf.GetInt32("port", 3306)),
 		Db:       conf.GetString("db"),
@@ -51,10 +62,9 @@ func Query(ctx context.Context, conf config.Configuration) (err error) {
 		Location: conf.GetString("loc"),
 	}
 
-	driver := conf.GetString("driver", "mysql")
 	sqlQuery := conf.GetString("sql")
 
-	db, err := sql.Open(driver, sqlConf.DSN())
+	db, err := sql.Open(sqlConf.Driver, sqlConf.DSN())
 
 	if err != nil {
 		return
@@ -82,6 +92,7 @@ func Exec(ctx context.Context, conf config.Configuration) (err error) {
 	}
 
 	sqlConf := sqlConfig{
+		Driver:   conf.GetString("driver", "mysql"),
 		Host:     conf.GetString("host", "localhost"),
 		Port:     int(conf.GetInt32("port", 3306)),
 		Db:       conf.GetString("db"),
@@ -91,30 +102,69 @@ func Exec(ctx context.Context, conf config.Configuration) (err error) {
 		Location: conf.GetString("loc"),
 	}
 
-	driver := conf.GetString("driver", "mysql")
-	sqlQuery := conf.GetString("sql")
+	sqlExec := conf.GetString("sql")
 
-	db, err := sql.Open(driver, sqlConf.DSN())
+	db, err := sql.Open(sqlConf.Driver, sqlConf.DSN())
 
 	if err != nil {
 		return
 	}
 
-	tx, err := db.Begin()
-	if err != nil {
-		return
-	}
+	isTrans := conf.GetBoolean("tx", true)
 
-	_, err = gosqljson.ExecTx(tx, sqlQuery)
-	if err != nil {
-		tx.Rollback()
-		return
-	}
+	sqls := strings.Split(sqlExec, ";")
 
-	err = tx.Commit()
-	if err != nil {
-		return
+	if isTrans {
+
+		var tx *sql.Tx
+		tx, err = db.Begin()
+		if err != nil {
+			return
+		}
+
+		for i := 0; i < len(sqls); i++ {
+			sql := trimSQL(sqls[i])
+			if len(sql) == 0 {
+				continue
+			}
+			_, err = gosqljson.ExecTx(tx, sql)
+			if err != nil {
+				tx.Rollback()
+				return
+			}
+		}
+
+		err = tx.Commit()
+		if err != nil {
+			return
+		}
+	} else {
+		for i := 0; i < len(sqls); i++ {
+			sql := trimSQL(sqls[i])
+			if len(sql) == 0 {
+				continue
+			}
+			_, err = gosqljson.ExecDb(db, sql)
+			if err != nil {
+				return
+			}
+		}
 	}
 
 	return
+}
+
+func trimSQL(sql string) string {
+	sql = strings.TrimSuffix(sql, "\n")
+	sql = strings.TrimSuffix(sql, ";")
+
+	sql = strings.TrimSpace(sql)
+
+	if len(sql) == 0 {
+		return ""
+	}
+
+	sql = sql + ";"
+
+	return sql
 }
